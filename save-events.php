cb1e1222e90402fe5fb4f72b262eb6ec7818b7ca
1qawsej1 <jarrettnovel@gmail.com>
2024-07-10 21:14:01 +0800

    Update save-events.php

--- save-events.php
+++ save-events.php
@@ -1,15 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = file_get_contents('php://input');
-    if (json_decode($data) !== null) {
-        file_put_contents('events.json', $data);
-        echo 'Events saved successfully';
-    } else {
-        http_response_code(400);
-        echo 'Invalid JSON';
-    }
-} else {
-    http_response_code(405);
-    echo 'Method Not Allowed';
-}
+    file_put_contents('events.json', $data);
+    echo 'Events saved successfully';
 ?>
